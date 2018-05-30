090e94e40229b40e072c9c1bd33c921cedee8472
LAPTOP-HBV2O4FT\user <mvp810624@yahoo.com.tw>
2018-05-31 00:20:10 +0800

    corollary of combination all subsets

--- done/Backtracking/combination_allsubsets.js
+++ done/Backtracking/combination_allsubsets.js
@@ -1,17 +1,13 @@
-class Combinations {
+class CombinationsIncludeSubsets {
     constructor(size, pick) {
-        if(size < 1)
+        if (size < 1)
             size = 1;
-        if(pick < 1)
+        if (pick < 1)
             pick = 1;
         this.size = Math.floor(size);
-        this.pick = Math.floor(pick);
-        this.AllPossible = [];                
+        this.maxpick = Math.floor(pick);
+        this.AllPossible = [];
         this.array = [];
-
-        for (var i = 0; i < this.pick; i++) {
-            this.array.push(0);
-        }
     }
     HasNext(index, num) {
         for (var i = 0; i < index; i++) {
@@ -28,31 +24,51 @@ class Combinations {
         return -1;
     }
 
-    start() {
+    IsAllZero() {
+        for (var i = 0; i < this.array.length; i++) {
+            if (this.array[i] != 0)
+                return false;
+        }
+        return true;
+    }
+    CollectParticularSizeSubsets(num) {
         var index = this.NewPosition();
-        if (index == -1) {   
+        if (index == -1) {
             var newarray = [];
-            for(var i = 0; i < this.array.length; i++)
+            for (var i = 0; i < this.array.length; i++)
                 newarray.push(this.array[i]);
             this.AllPossible.push(newarray);
+
             return;
         }
 
         for (var i = 1; i <= this.size; i++) {
             if (this.HasNext(index, i)) {
                 this.array[index] = i;
-                this.start(); 
+                this.CollectParticularSizeSubsets(num);
                 this.array[index] = 0;
             }
         }
         return;
     }
 
-    AllResult(){
+    start() {
+        for (var max = 1; max <= this.maxpick; max++) {
+            this.array = [];
+            for (var i = 0; i < max; i++) {
+                this.array.push(0);
+            }
+            this.CollectParticularSizeSubsets(max);
+        }
+
+    }
+
+    AllResult() {
         return this.AllPossible;
     }
 }
 
-var combination_generate = new Combinations(3, 2);
+var combination_generate = new CombinationsIncludeSubsets(7, 5);
 combination_generate.start();
-console.log(combination_generate.AllResult());
+var all = combination_generate.AllResult();
+console.log(all.length);
