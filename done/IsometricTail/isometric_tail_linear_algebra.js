ebd6a75f998bf7013d46210882df6d581f6b6106
LAPTOP-HBV2O4FT\user <mvp810624@yahoo.com.tw>
2019-11-17 12:29:14 +0800

    Make diamond has 3D effect.

--- done/IsometricTail/isometric_tail_linear_algebra.js
+++ done/IsometricTail/isometric_tail_linear_algebra.js
@@ -22,9 +22,9 @@ class DiamondBoard {
         canvas.setAttribute('height', String(h));
 
         canvas.setAttribute("style", "position: absolute;" +
-                                                "top:" + topOffset + ";" +
-                                                "left:" + leftOffset + ";" +
-                                                "z-index: 0");
+            "top:" + topOffset + ";" +
+            "left:" + leftOffset + ";" +
+            "z-index: 0");
 
         canvas.addEventListener("mousemove", moveDetect);
         canvas.addEventListener("mousedown", pressDetect);
@@ -32,9 +32,9 @@ class DiamondBoard {
     }
 
     initDiamonds(canvas) {
-        for(var i = 0; i < this.diagonalNum; i++) {
+        for (var i = 0; i < this.diagonalNum; i++) {
             this.diamonds.push([]);
-            for(var j = 0; j < this.diagonalNum; j++) {
+            for (var j = 0; j < this.diagonalNum; j++) {
                 var diamond = {
                     x: j,
                     y: i,
@@ -45,12 +45,12 @@ class DiamondBoard {
             }
         }
 
-        for(var i = 0; i < this.diamonds.length; i++) {
-            for(var j = 0; j < this.diamonds[i].length; j++) {
+        for (var i = 0; i < this.diamonds.length; i++) {
+            for (var j = 0; j < this.diamonds[i].length; j++) {
                 this.drawDiamond(canvas, this.diamonds[i][j]);
             }
         }
-        
+
     }
 
     drawNormalRect(canvas, diamond) {
@@ -60,11 +60,11 @@ class DiamondBoard {
         ctx.strokeStyle = borderColor;
         ctx.fillStyle = bgColor;
         ctx.beginPath();
-        ctx.moveTo( diamond.x      * this.width,   diamond.y      * this.height);
-        ctx.lineTo((diamond.x + 1) * this.width,   diamond.y      * this.height);
-        ctx.lineTo((diamond.x + 1) * this.width,  (diamond.y + 1) * this.height);
-        ctx.lineTo( diamond.x      * this.width , (diamond.y + 1) * this.height);
-        ctx.lineTo( diamond.x      * this.width ,  diamond.y      * this.height);
+        ctx.moveTo(diamond.x * this.width, diamond.y * this.height);
+        ctx.lineTo((diamond.x + 1) * this.width, diamond.y * this.height);
+        ctx.lineTo((diamond.x + 1) * this.width, (diamond.y + 1) * this.height);
+        ctx.lineTo(diamond.x * this.width, (diamond.y + 1) * this.height);
+        ctx.lineTo(diamond.x * this.width, diamond.y * this.height);
         ctx.fill();
         ctx.stroke();
         ctx.closePath();
@@ -72,15 +72,15 @@ class DiamondBoard {
 
     getDiamondCoord(x, y) {
         return {
-                    i: (x - y)*this.width / 2,
-                    j: (x + y)*this.height / 2
-               }
+            i: (x - y) * this.width / 2,
+            j: (x + y) * this.height / 2
+        }
     }
 
     drawDiamond(canvas, diamond) {
         this.drawDiamondBackground(canvas, diamond);
-        
-        if(diamond.state == "click")
+        this.draw3DEffect(canvas, diamond);
+        if (diamond.state == "click")
             this.drawDiamondContent(canvas, diamond);
     }
 
@@ -103,7 +103,6 @@ class DiamondBoard {
         ctx.lineTo(offsetX + pt2.i, pt2.j);
         ctx.lineTo(offsetX + pt3.i, pt3.j);
         ctx.lineTo(offsetX + pt0.i, pt0.j);
-
         ctx.fill();
         ctx.stroke();
         ctx.closePath();
@@ -112,38 +111,141 @@ class DiamondBoard {
     drawDiamondContent(canvas, diamond) {
         var ctx = canvas.getContext("2d");
         var offsetX = this.diagonalNum * this.width / 2;
+        var height_effect3d = this.height / 10;
         var pt0 = this.getDiamondCoord(diamond.x, diamond.y);
         var pt2 = this.getDiamondCoord(diamond.x + 1, diamond.y + 1);
-        
+        var length = 0;
         ctx.save();
         ctx.beginPath();
         ctx.translate((pt0.i + pt2.i) / 2 + offsetX, (pt0.j + pt2.j) / 2);
-        if(this.height > this.width) {
+        if (this.height > this.width) {
             ctx.scale(this.width / this.height, 1);
-            ctx.arc(0, 0, this.width / 3, 0, Math.PI * 2);
+            length = this.width / 3;
+            ctx.arc(0, 0, length, 0, Math.PI * 2);
         }
         else {
             ctx.scale(1, this.height / this.width);
-            ctx.arc(0, 0, this.height / 3, 0, Math.PI * 2);
+            length = this.height / 3;
+            ctx.arc(0, 0, length, 0, Math.PI * 2);
         }
-        
         ctx.fillStyle = diamond.drawColor;
         ctx.fill();
         ctx.closePath();
         ctx.restore();
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.translate((pt0.i + pt2.i) / 2 + offsetX, (pt0.j + pt2.j) / 2 - height_effect3d);
+        if (this.height > this.width) {
+            ctx.scale(this.width / this.height, 1);
+            ctx.arc(0, 0, length, 0, Math.PI * 2);
+        }
+        else {
+            ctx.scale(1, this.height / this.width);
+            ctx.arc(0, 0, length, 0, Math.PI * 2);
+        }
+        ctx.fillStyle = diamond.drawColor;
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+
+
+        var pt1 = this.getDiamondCoord(diamond.x + 1, diamond.y);
+        var pt3 = this.getDiamondCoord(diamond.x, diamond.y + 1);
+
+        ctx.beginPath();
+        ctx.moveTo((pt1.i + pt3.i) / 2 + offsetX - length, (pt0.j + pt2.j) / 2);
+        ctx.lineTo((pt1.i + pt3.i) / 2 + offsetX + length, (pt0.j + pt2.j) / 2);
+        ctx.lineTo((pt1.i + pt3.i) / 2 + offsetX + length, (pt0.j + pt2.j) / 2 - height_effect3d);
+        ctx.lineTo((pt1.i + pt3.i) / 2 + offsetX - length, (pt0.j + pt2.j) / 2 - height_effect3d);
+        ctx.lineTo((pt1.i + pt3.i) / 2 + offsetX - length, (pt0.j + pt2.j) / 2);
+        ctx.fillStyle = diamond.drawColor;
+        ctx.fill();
+
+        ctx.closePath();
+
     }
 
+    draw3DEffect(canvas, diamond) {
+        var ctx = canvas.getContext("2d");
+        ctx.strokeStyle = "#ffffff";
+        ctx.fillStyle = "#cccccc";
+
+        var offsetX = this.diagonalNum * this.width / 2;
+        var pt1 = this.getDiamondCoord(diamond.x + 1, diamond.y);
+        var pt2 = this.getDiamondCoord(diamond.x + 1, diamond.y + 1);
+        var pt3 = this.getDiamondCoord(diamond.x, diamond.y + 1);
+
+        ctx.beginPath();
+        ctx.moveTo(offsetX + pt1.i, pt1.j);
+        ctx.lineTo(offsetX + pt2.i, pt2.j);
+        ctx.lineTo(offsetX + pt2.i, pt2.j + this.height / 2);
+        ctx.lineTo(offsetX + pt1.i, pt1.j + this.height / 2);
+        ctx.lineTo(offsetX + pt1.i, pt1.j);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.moveTo(offsetX + pt3.i, pt3.j);
+        ctx.lineTo(offsetX + pt2.i, pt2.j);
+        ctx.lineTo(offsetX + pt2.i, pt2.j + this.height / 2);
+        ctx.lineTo(offsetX + pt3.i, pt3.j + this.height / 2);
+        ctx.lineTo(offsetX + pt3.i, pt3.j);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    recoverSurface(canvas, diamond) {
+        this.drawDiamondBackground(canvas, diamond);
+        if (diamond.state == "click")
+            this.drawDiamondContent(canvas, diamond);
+
+        var ctx = canvas.getContext("2d");
+        ctx.strokeStyle = "#ffffff";
+        ctx.fillStyle = "#cccccc";
+
+        var offsetX = this.diagonalNum * this.width / 2;
+        var pt1 = this.getDiamondCoord(diamond.x + 1, diamond.y);
+        var pt2 = this.getDiamondCoord(diamond.x + 1, diamond.y + 1);
+        var pt3 = this.getDiamondCoord(diamond.x, diamond.y + 1);
+
+        if (diamond.x == this.diamonds.length - 1) {
+            ctx.beginPath();
+            ctx.moveTo(offsetX + pt1.i, pt1.j);
+            ctx.lineTo(offsetX + pt2.i, pt2.j);
+            ctx.lineTo(offsetX + pt2.i, pt2.j + this.height / 2);
+            ctx.lineTo(offsetX + pt1.i, pt1.j + this.height / 2);
+            ctx.lineTo(offsetX + pt1.i, pt1.j);
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+        }
+
+        if (diamond.y == this.diamonds.length - 1) {
+            ctx.beginPath();
+            ctx.moveTo(offsetX + pt3.i, pt3.j);
+            ctx.lineTo(offsetX + pt2.i, pt2.j);
+            ctx.lineTo(offsetX + pt2.i, pt2.j + this.height / 2);
+            ctx.lineTo(offsetX + pt3.i, pt3.j + this.height / 2);
+            ctx.lineTo(offsetX + pt3.i, pt3.j);
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
     getCoord(x, y) {
         var x0 = x - this.diagonalNum * this.width / 2;;
         var y0 = y;
 
         var column = Math.floor(x0 / this.width + y0 / this.height);
-        var row    = Math.floor(y0 / this.height - x0 / this.width);
+        var row = Math.floor(y0 / this.height - x0 / this.width);
 
-        return { 
-                 i: (row),
-                 j: (column) 
-               };
+        return {
+            i: (row),
+            j: (column)
+        };
     }
 }
 
@@ -155,27 +257,36 @@ function moveDetect(e) {
     var mx = e.pageX - canvas.offsetLeft;
     var my = e.pageY - canvas.offsetTop;
 
-    var coord = board.getCoord(mx,my);
+    var coord = board.getCoord(mx, my);
     var i0 = coord.i;
     var j0 = coord.j;
 
-    if(i0 >= 0 && j0 >= 0) {
-        if(i0 < board.diamonds.length && j0 < board.diamonds[i0].length) {
-            if (board.diamonds[i0][j0].state == "click") {
-                return;
-            }
-            else {
+    if (i0 >= 0 && j0 >= 0) {
+        if (i0 < board.diamonds.length && j0 < board.diamonds[i0].length) {
+            var is_click = board.diamonds[i0][j0].state == "click";
+            if (!is_click) {
                 board.diamonds[i0][j0].state = "hover";
+            }
 
-                if(i0 != last_i0 || j0 != last_j0)
-                    if(last_i0 != -1 && last_j0 != -1)
-                        if(board.diamonds[last_i0][last_j0].state == "hover") {
-                            board.diamonds[last_i0][last_j0].state = "normal";
-                            board.drawDiamond(canvas, board.diamonds[last_i0][last_j0]);
-                        }
-                        
+            var is_last_coord_different = (i0 != last_i0 || j0 != last_j0) &&
+                (last_i0 != -1 && last_j0 != -1);
+            if (is_last_coord_different)
+                if (board.diamonds[last_i0][last_j0].state == "hover") {
+                    board.diamonds[last_i0][last_j0].state = "normal";
+                    board.recoverSurface(canvas, board.diamonds[last_i0][last_j0]);
+
+                    if (last_i0 + 1 < board.diamonds.length)
+                        board.recoverSurface(canvas, board.diamonds[last_i0 + 1][last_j0]);
+                    if (last_j0 + 1 < board.diamonds[last_i0].length)
+                        board.recoverSurface(canvas, board.diamonds[last_i0][last_j0 + 1]);
+                    if (last_i0 + 1 < board.diamonds.length &&
+                        last_j0 + 1 < board.diamonds[last_i0 + 1].length)
+                        board.recoverSurface(canvas, board.diamonds[last_i0 + 1][last_j0 + 1]);
+                }
+
+            if (!is_click) {
                 board.drawDiamond(canvas, board.diamonds[i0][j0]);
-                
+
                 last_i0 = i0;
                 last_j0 = j0;
             }
@@ -200,8 +311,8 @@ function pressDetect(e) {
         }
     }
 
-    if(i0 >= 0 && j0 >= 0) {
-        if(i0 < board.diamonds.length && j0 < board.diamonds[i0].length) {
+    if (i0 >= 0 && j0 >= 0) {
+        if (i0 < board.diamonds.length && j0 < board.diamonds[i0].length) {
             if (board.diamonds[i0][j0].state == "click") {
                 board.diamonds[i0][j0].state = "normal";
             }
@@ -211,7 +322,22 @@ function pressDetect(e) {
                 board.diamonds[i0][j0].state = "click";
                 board.diamonds[i0][j0].drawColor = nextColor;
             }
+
             board.drawDiamond(canvas, board.diamonds[i0][j0]);
+
+            if (i0 + 1 < board.diamonds.length) {
+                //board.diamonds[i0+1][j0].state = "normal";
+                board.recoverSurface(canvas, board.diamonds[i0 + 1][j0]);
+            }
+            if (j0 + 1 < board.diamonds.length) {
+                //board.diamonds[i0][j0+1].state = "normal";
+                board.recoverSurface(canvas, board.diamonds[i0][j0 + 1]);
+            }
+            if (i0 + 1 < board.diamonds.length &&
+                j0 + 1 < board.diamonds[i0 + 1].length) {
+                //board.diamonds[i0+1][j0+1].state = "normal";
+                board.recoverSurface(canvas, board.diamonds[i0 + 1][j0 + 1]);
+            }
         }
     }
 }
