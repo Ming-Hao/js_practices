0c494d4d26cbb7bb52ca6c5931fd99cdf1931cd7
minghao <minghao@minghaodeMac-mini.local>
2023-10-08 19:29:38 +0800

    Add BounceBall(Horizontal) practice

--- done/BounceBall/Ball.js
+++ done/BounceBall/Ball.js
@@ -2,20 +2,33 @@ class Ball {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.velocity = 0;
         this.radius =  Math.random() * (20 - 5) + 5;
+        this.velocityX = 0; 
+        this.velocityY = 0; 
     }
-    updateVelocity(gravity, groundY) {
+    updateVelocity(gravity, groundY, boundLeft, boundRight) {
         if (this.y >= groundY) {
             this.y = groundY;
+            this.velocityX = 0;
+            this.velocityY = 0;
             return;
         }
-        this.velocity = this.velocity + gravity;
-        this.y = this.y - this.velocity;
+        this.velocityY = this.velocityY + gravity;
+        this.y = this.y - this.velocityY;
+
+        if (this.x > boundRight) {
+            this.velocityX = this.velocityX * (-1);
+        }
+        else if (this.x < boundLeft) {
+            this.velocityX = this.velocityX * (-1);
+        }
+        this.x = this.x + this.velocityX
     }
-    startBounce(gravity) {
-        this.velocity = Math.random() * (20 - 10) + 10;
-        this.velocity = this.velocity + gravity;
-        this.y = this.y - this.velocity;
+    startBounce(gravity, velocityX = 0, velocityY = 0) {
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.velocityY = this.velocityY + gravity;
+        this.y = this.y - this.velocityY;
+        this.x = this.x + this.velocityX;
     }
 }
